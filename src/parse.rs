52db20c96018541a1de7f63f515890a7e7bf171f
Judah Rand <17158624+judahrand@users.noreply.github.com>
2022-04-12 08:43:26 +0100

    Format code

--- src/parse.rs
+++ src/parse.rs
@@ -1,28 +1,33 @@
 use std::io::BufRead;
 use std::ops::Add;
 
-use chrono::{DateTime, Duration};
+use bytes::Buf;
 use chrono::prelude::*;
 use chrono::Utc;
-use bytes::Buf;
-
+use chrono::{DateTime, Duration};
 
 pub trait Decoder: Buf {
     fn get_bool(&mut self) -> bool;
-    fn get_string(&mut self) -> String where Self: Sized;
+    fn get_string(&mut self) -> String
+    where
+        Self: Sized;
     fn get_timestamp(&mut self) -> DateTime<Utc>;
     fn get_rowinfo(&mut self, byte: char) -> bool;
     fn get_tupledata(&mut self) -> Vec<Tuple>;
-    fn get_columns(&mut self) -> Vec<Column> where Self: Sized;
+    fn get_columns(&mut self) -> Vec<Column>
+    where
+        Self: Sized;
 }
-
 
 impl Decoder for &[u8] {
     fn get_bool(&mut self) -> bool {
         self.get_u8() != 0
     }
 
-    fn get_string(&mut self) -> String where Self: Sized {
+    fn get_string(&mut self) -> String
+    where
+        Self: Sized,
+    {
         let mut buf = vec![];
         self.reader().read_until(0, &mut buf).unwrap();
         buf.pop();
@@ -41,8 +46,8 @@ impl Decoder for &[u8] {
             true => {
                 self.advance(1);
                 true
-            },
-            false => false
+            }
+            false => false,
         }
     }
 
@@ -52,106 +57,105 @@ impl Decoder for &[u8] {
         for _ in 0..size {
             let flag = self.get_u8() as char;
             let vsize = self.get_u32() as usize;
-            data.push(
-                Tuple {
-                    flag: flag as char,
-                    value: (&self.chunk()[0..vsize]).to_vec()
-                }
-            );
+            data.push(Tuple {
+                flag: flag as char,
+                value: (&self.chunk()[0..vsize]).to_vec(),
+            });
         }
         data
     }
 
-    fn get_columns(&mut self) -> Vec<Column> where Self: Sized {
+    fn get_columns(&mut self) -> Vec<Column>
+    where
+        Self: Sized,
+    {
         let size = self.get_u16();
         let mut data = Vec::<Column>::with_capacity(size as usize);
         for _ in 0..size {
-            data.push(
-                Column {
-                    key: self.get_bool(),
-                    name: self.get_string(),
-                    r#type: self.get_u32(),
-                    mode: self.get_u32(),
-                }
-            );
+            data.push(Column {
+                key: self.get_bool(),
+                name: self.get_string(),
+                r#type: self.get_u32(),
+                mode: self.get_u32(),
+            });
         }
         data
     }
 }
 
 pub struct Begin {
-	// The final LSN of the transaction.
-	lsn: u64,
-	// Commit timestamp of the transaction. The value is in number of
-	// microseconds since PostgreSQL epoch (2000-01-01).
-	timestamp: DateTime<Utc>,
-	// Xid of the transaction.
-	xid: i32,
+    // The final LSN of the transaction.
+    lsn: u64,
+    // Commit timestamp of the transaction. The value is in number of
+    // microseconds since PostgreSQL epoch (2000-01-01).
+    timestamp: DateTime<Utc>,
+    // Xid of the transaction.
+    xid: i32,
 }
 
 pub struct Commit {
-	flags: u8,
-	// The final LSN of the transaction.
-	lsn: u64,
-	// The final LSN of the transaction.
-	transaction_lsn: u64,
-	timestamp: DateTime<Utc>,
+    flags: u8,
+    // The final LSN of the transaction.
+    lsn: u64,
+    // The final LSN of the transaction.
+    transaction_lsn: u64,
+    timestamp: DateTime<Utc>,
 }
 
 pub struct Relation {
-	// ID of the relation.
-	id: u32,
-	// Namespace (empty string for pg_catalog).
-	namespace: String,
-	name: String,
-	replica: u8,
-	columns: Vec<Column>,
+    // ID of the relation.
+    id: u32,
+    // Namespace (empty string for pg_catalog).
+    namespace: String,
+    name: String,
+    replica: u8,
+    columns: Vec<Column>,
 }
 
 impl Relation {
     fn IsEmpty(&self) -> bool {
-	    self.id == 0 && self.name.is_empty() && self.replica == 0 && self.columns.is_empty()
+        self.id == 0 && self.name.is_empty() && self.replica == 0 && self.columns.is_empty()
     }
 }
 
 pub struct Type {
-	// ID of the data type
-	id: u32,
-	namespace: String,
-	name: String,
+    // ID of the data type
+    id: u32,
+    namespace: String,
+    name: String,
 }
 
 pub struct Insert {
-	/// ID of the relation corresponding to the ID in the relation message.
-	relation_id: u32,
-	// Identifies the following TupleData message as a new tuple.
-	new: bool,
-	row: Vec<Tuple>,
+    /// ID of the relation corresponding to the ID in the relation message.
+    relation_id: u32,
+    // Identifies the following TupleData message as a new tuple.
+    new: bool,
+    row: Vec<Tuple>,
 }
 
 pub struct Update {
-	/// ID of the relation corresponding to the ID in the relation message.
-	relation_id: u32,
-	// Identifies the following TupleData message as a new tuple.
-	old: bool,
-	key: bool,
-	new: bool,
-	old_row: Vec<Tuple>,
-	row: Vec<Tuple>,
+    /// ID of the relation corresponding to the ID in the relation message.
+    relation_id: u32,
+    // Identifies the following TupleData message as a new tuple.
+    old: bool,
+    key: bool,
+    new: bool,
+    old_row: Vec<Tuple>,
+    row: Vec<Tuple>,
 }
 
 pub struct Delete {
-	/// ID of the relation corresponding to the ID in the relation message.
-	relation_id: u32,
-	// Identifies the following TupleData message as a new tuple.
-	key: bool,
-	old: bool,
-	row: Vec<Tuple>,
+    /// ID of the relation corresponding to the ID in the relation message.
+    relation_id: u32,
+    // Identifies the following TupleData message as a new tuple.
+    key: bool,
+    old: bool,
+    row: Vec<Tuple>,
 }
 
 pub struct Origin {
-	lsn: u64,
-	name: String,
+    lsn: u64,
+    name: String,
 }
 
 // TODO: Add support for more Postgres types
@@ -162,14 +166,14 @@ pub struct Origin {
 
 pub struct Column {
     key: bool,
-	name: String,
-	r#type: u32,
-	mode: u32,
+    name: String,
+    r#type: u32,
+    mode: u32,
 }
 
 pub struct Tuple {
     flag: char,
-    value: Vec<u8>
+    value: Vec<u8>,
 }
 
 pub enum LogicalReplicationMessage {
@@ -189,54 +193,52 @@ fn parse(src: &[u8]) -> Result<LogicalReplicationMessage, String> {
     let msg_type = src[0] as char;
     let mut buf = &src[1..];
     match msg_type {
-        'B' => Ok(LogicalReplicationMessage::Begin(
-            Begin { lsn: buf.get_u64(), timestamp: buf.get_timestamp(), xid: buf.get_i32() }
-        )),
-        'C' => Ok(LogicalReplicationMessage::Commit(
-            Commit {
-                flags: buf.get_u8(),
-                lsn: buf.get_u64(),
-                transaction_lsn: buf.get_u64(),
-                timestamp: buf.get_timestamp()
-            }
-        )),
-        'O' => Ok(LogicalReplicationMessage::Origin(
-            Origin { lsn: buf.get_u64(), name: buf.get_string() }
-        )),
-        'R' => Ok(LogicalReplicationMessage::Relation(
-            Relation {
-                id: buf.get_u32(),
-                namespace: buf.get_string(),
-                name: buf.get_string(),
-                replica: buf.get_u8(),
-                columns: buf.get_columns()
-            }
-        )),
-        'Y' => Ok(LogicalReplicationMessage::Type(
-            Type { id: buf.get_u32(), namespace: buf.get_string(), name: buf.get_string() }
-        )),
-        'I' => Ok(LogicalReplicationMessage::Insert(
-            Insert { relation_id: buf.get_u32(), new: buf.get_bool(), row: buf.get_tupledata() }
-        )),
-        'U' => Ok(LogicalReplicationMessage::Update(
-            Update {
-                relation_id: buf.get_u32(),
-                key: buf.get_rowinfo('K'),
-                old: buf.get_rowinfo('O'),
-                old_row: buf.get_tupledata(),
-                new: buf.get_bool(),
-                row: buf.get_tupledata(),
-            }
-        )),
-        'D' => Ok(LogicalReplicationMessage::Delete(
-            Delete {
-                relation_id: buf.get_u32(),
-                key: buf.get_rowinfo('K'),
-                old: buf.get_rowinfo('O'),
-                row: buf.get_tupledata(),
-            }
-
-        )),
-        _ => Err(format!("Unknown message type {}", msg_type))
+        'B' => Ok(LogicalReplicationMessage::Begin(Begin {
+            lsn: buf.get_u64(),
+            timestamp: buf.get_timestamp(),
+            xid: buf.get_i32(),
+        })),
+        'C' => Ok(LogicalReplicationMessage::Commit(Commit {
+            flags: buf.get_u8(),
+            lsn: buf.get_u64(),
+            transaction_lsn: buf.get_u64(),
+            timestamp: buf.get_timestamp(),
+        })),
+        'O' => Ok(LogicalReplicationMessage::Origin(Origin {
+            lsn: buf.get_u64(),
+            name: buf.get_string(),
+        })),
+        'R' => Ok(LogicalReplicationMessage::Relation(Relation {
+            id: buf.get_u32(),
+            namespace: buf.get_string(),
+            name: buf.get_string(),
+            replica: buf.get_u8(),
+            columns: buf.get_columns(),
+        })),
+        'Y' => Ok(LogicalReplicationMessage::Type(Type {
+            id: buf.get_u32(),
+            namespace: buf.get_string(),
+            name: buf.get_string(),
+        })),
+        'I' => Ok(LogicalReplicationMessage::Insert(Insert {
+            relation_id: buf.get_u32(),
+            new: buf.get_bool(),
+            row: buf.get_tupledata(),
+        })),
+        'U' => Ok(LogicalReplicationMessage::Update(Update {
+            relation_id: buf.get_u32(),
+            key: buf.get_rowinfo('K'),
+            old: buf.get_rowinfo('O'),
+            old_row: buf.get_tupledata(),
+            new: buf.get_bool(),
+            row: buf.get_tupledata(),
+        })),
+        'D' => Ok(LogicalReplicationMessage::Delete(Delete {
+            relation_id: buf.get_u32(),
+            key: buf.get_rowinfo('K'),
+            old: buf.get_rowinfo('O'),
+            row: buf.get_tupledata(),
+        })),
+        _ => Err(format!("Unknown message type {}", msg_type)),
     }
 }
